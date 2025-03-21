a81885c0d27cc7d20e3e9979ec860210ef3cffb5
thiago2436 <thiagomartins.florestal@gmail.com>
2025-03-21 09:41:56 -0300

    update test file

--- index.test.js
+++ index.test.js
@@ -1,6 +1,16 @@
 const request = require("supertest");
 const app = require("./index");
 
+let server;
+
+beforeAll(() => {
+  server = app.listen(3000, () => console.log("Servidor iniciado para testes"));
+});
+
+afterAll((done) => {
+  server.close(done); // Fecha o servidor corretamente após os testes
+});
+
 describe("Testando a API", () => {
   test("GET / deve retornar Olá, mundo!", async () => {
     const res = await request(app).get("/");
@@ -8,4 +18,3 @@ describe("Testando a API", () => {
     expect(res.text).toBe("Olá, mundo!");
   });
 });
-
